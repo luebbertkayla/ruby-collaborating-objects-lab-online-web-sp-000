673d423ef8442f4ccd14a59debb1ca8c447c7c0a
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-14 20:17:23 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,17 @@
 class MP3Importer
   
-  attr_accessor :file_path
-  
-  def initialize(file_path)
-    @file_path = file_path
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
   end
+
+  def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+  end
+
+  def import
+    files.each {|song| Song.new_by_filename(song)}
+  end
+
 end
