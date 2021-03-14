9ae5532e0379603f6ba57ca056ef70b3c9912a14
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-14 19:56:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name 
   
-  @song_count = 0 
+  @@song_count = 0 
   
   def initialize(name)
     @name = name 
@@ -11,6 +11,9 @@ class Artist
   def add_song(song)
     @songs << song 
     song.artist = self
+    @@song_count += 1 
+  end
+    
     
     
 end
